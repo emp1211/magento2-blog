4a5958c08adb8f39ef2190233058e96392ac7c9e
emp1211 <erikpaffett@gmail.com>
2024-11-28 01:40:30 -0500

    Add Model, ResourceModel, and Collection classes for Post entity

---
+++ Model/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlueHeron\Blog\Model;
+
+use Magento\Framework\Model\AbstractModel;
+use BlueHeron\Blog\Model\ResourceModel\Post as PostResource;
+
+class Post extends AbstractModel
+{
+    private function _construct()
+    {
+        $this->_init(PostResource::class);
+    }
+}
---
+++ Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,17 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace BlueHeron\Blog\Model\ResourceModel\Post;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use BlueHeron\Blog\Model\Post;
+use BlueHeron\Blog\Model\ResourceModel\Post as PostResource;
+
+class Collection extends AbstractCollection
+{
+    private function _construct()
+    {
+        $this->_init(Post::class, PostResource::class);
+    }
+}
