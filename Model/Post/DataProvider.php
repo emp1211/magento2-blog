844bad01b930f9745ef47409ab373a381a705199
emp1211 <erikpaffett@gmail.com>
2024-11-30 14:03:27 -0500

    Create Edit Page route and admin form for add new post

---
+++ Model/Post/DataProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlueHeron\Blog\Model\Post;
+
+use BlueHeron\Blog\Model\ResourceModel\Post\CollectionFactory;
+use Magento\Ui\DataProvider\Modifier\PoolInterface;
+use Magento\Ui\DataProvider\ModifierPoolDataProvider;
+
+
+class DataProvider extends ModifierPoolDataProvider
+{
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        array $meta = [],
+        array $data = [],
+        PoolInterface $pool = null
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data, $pool);
+        $this->collection = $collectionFactory->create();
+    }
+}
